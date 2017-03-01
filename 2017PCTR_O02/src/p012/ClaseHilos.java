a1a0907898564777f12bea9dc4cb745ae045c227
CarolinaCCZ <ccz0001@alu.ubu.es>
2017-03-01 02:25:36 +0100

    Clase Hilos

---
+++ 2017PCTR_O02/src/p012/ClaseHilos.java
@@ -0,0 +1,28 @@
+package p012;
+
+import java.util.concurrent.TimeUnit;
+
+public class ClaseHilos extends Thread {
+	
+	protected  Board board;
+	protected Ball ball;
+	
+	public ClaseHilos(Ball ball, Board board) {
+		this.board = board;
+		this.ball = ball;
+	}
+
+	public void run() {
+		try {
+			while (true) {
+				ball.move();
+				board.repaint();
+
+				TimeUnit.MILLISECONDS.sleep(1000); // Un segundo
+			}
+		} catch (InterruptedException e) {
+			return;
+		}
+
+	}
+}
